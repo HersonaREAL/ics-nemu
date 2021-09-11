923160bcf8659d966b20aaa571144f307ae48dbe
tracer-ics2021 <tracer@njuics.org>
2021-09-11 20:55:36 +0800

    >  compile 201220000    Static hostname: cc-HP-EliteBook-735-G5          Icon name: computer-laptop            Chassis: laptop         Machine ID: 634ede29c26b4f849b82b75204506050            Boot ID: 4ccbae9aa28d4b1784ba6f848449b01a   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  20:55:36 up  3:32,  2 users,  load average: 1.13, 0.88, 0.82

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -3,6 +3,7 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include "sdb.h"
+#include "utils.h"
 
 static int is_batch_mode = false;
 
@@ -34,6 +35,7 @@ static int cmd_c(char *args) {
 
 
 static int cmd_q(char *args) {
+  nemu_state.state = NEMU_END;
   return -1;
 }
 
