83e8c861be5b7c752515086f5c3215be3106d551
tracer-ics2021 <tracer@njuics.org>
2021-09-12 16:57:53 +0800

    >  compile 201220000    Static hostname: cc-HP-EliteBook-735-G5          Icon name: computer-laptop            Chassis: laptop         Machine ID: 634ede29c26b4f849b82b75204506050            Boot ID: a53da304e04a4ed5bdba92691ee33f4d   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-34-generic       Architecture: x86-64  16:57:53 up  2:12,  2 users,  load average: 0.50, 0.74, 0.98

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -10,9 +10,10 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
-  for (int i = 0; i < sizeof(regs); ++i) {
-    printf("a: \n" );
-  }
+  // for (int i = 0; i < sizeof(regs); ++i) {
+  //   printf("%s: \n",regs[i]);
+  // }
+  printf("%s\n",regs[0]);
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
