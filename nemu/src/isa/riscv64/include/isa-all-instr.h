ada0ff3edcde84005b5f8aab51fca81249271910
cc <jycdeya@gmail.com>
2021-10-08 16:10:26 +0800

    use llvm disassembler

--- nemu/src/isa/riscv64/include/isa-all-instr.h
+++ nemu/src/isa/riscv64/include/isa-all-instr.h
@@ -1,9 +1,6 @@
 #include <cpu/decode.h>
 #include "../local-include/rtl.h"
 
-#define INSTR_NULLARY(f) f(inv) f(nemu_trap)
-#define INSTR_UNARY(f)
-#define INSTR_BINARY(f) f(auipc) f(jal) f(lui)
-#define INSTR_TERNARY(f) f(ld) f(sd) f(addi) f(jalr) f(lw) f(addw) f(subw) f(sllw) f(srlw) f(sraw) f(add) f(sub) f(slti) f(sltiu) f(beq) f(bne) f(blt) f(bge) f(bltu) f(bgeu) f(addiw) f(slliw) f(srliw) f(sraiw) f(slli) f(srli) f(srai) f(sb) f(sh) f(sw) f(sll) f(slt) f(sltu) f(xor) f(srl) f(sra) f(or) f(and) f(xori) f(ori) f(andi) f(lb) f(lh) f(lbu) f(lhu) f(lwu) f(mulw) f(divw) f(divuw) f(remw) f(remuw) f(mul)
+#define INSTR_LIST(f) f(lui) f(lw) f(sw) f(inv) f(nemu_trap) f(ld) f(lb) f(lh) f(lbu) f(lhu) f(lwu) f(sd) f(sb) f(sh) f(addi) f(slti) f(sltiu) f(xori) f(ori) f(andi) f(slli) f(srli) f(srai) f(add) f(sub) f(sll) f(slt) f(sltu) f(xor) f(srl) f(sra) f(or) f(and) f(mul) f(addw) f(subw) f(sllw) f(srlw) f(sraw) f(mulw) f(divw) f(divuw) f(remw) f(remuw) f(beq) f(bne) f(blt) f(bge) f(bltu) f(bgeu) f(addiw) f(slliw) f(srliw) f(sraiw) f(auipc) f(jalr) f(jal)
 
 def_all_EXEC_ID();
