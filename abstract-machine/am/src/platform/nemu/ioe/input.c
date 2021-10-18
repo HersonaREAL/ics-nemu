abb25cc35c7876977cc8f9e1c0eef94a62e7e634
cc <jycdeya@gmail.com>
2021-10-18 21:58:33 +0800

    feat: keyborad

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -1,9 +1,16 @@
 #include <am.h>
+#include <bits/stdint-uintn.h>
 #include <nemu.h>
 
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  uint32_t keycode = inl(KBD_ADDR);
+  if (keycode & 0x8000) {
+    kbd->keydown = 1;
+    kbd->keycode = keycode & (~0x8000);
+  } else {
+    kbd->keydown = 0;
+    kbd->keycode = keycode;//AM_KEY_NONE;
+  }
 }
