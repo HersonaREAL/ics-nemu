dc0da963f91da0f695737af79eee68dc207f5a14
cc <jycdeya@gmail.com>
2021-10-19 21:58:40 +0800

    feat: audio

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -1,4 +1,5 @@
 #include <am.h>
+#include <bits/stdint-uintn.h>
 #include <nemu.h>
 
 #define AUDIO_FREQ_ADDR      (AUDIO_ADDR + 0x00)
@@ -7,20 +8,50 @@
 #define AUDIO_SBUF_SIZE_ADDR (AUDIO_ADDR + 0x0c)
 #define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
 #define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
+#define AUDIO_SBUF_START_ADDR (AUDIO_ADDR + 0x18)
+#define AUDIO_SBUF_END_ADDR (AUDIO_ADDR + 0x1c)
+
+static void audio_write(uint8_t *buf, int len) {
+  uint32_t i, used = inl(AUDIO_COUNT_ADDR), bufsz = inl(AUDIO_SBUF_SIZE_ADDR);
+  uint32_t end;
+  while (used + len > bufsz) {
+    used = inl(AUDIO_COUNT_ADDR);
+  }
+
+  end = inl(AUDIO_SBUF_END_ADDR);
+  for (i = 0; i < len; ++i) {
+    outb(AUDIO_SBUF_ADDR + end, buf[i]);
+    end = (end + 1)%bufsz;
+  }
+
+  outl(AUDIO_COUNT_ADDR, used + len);
+  outl(AUDIO_SBUF_END_ADDR,end);
+}
 
 void __am_audio_init() {
+  outl(AUDIO_SBUF_START_ADDR,0);
+  outl(AUDIO_SBUF_END_ADDR,0);
+  outl(AUDIO_SBUF_SIZE_ADDR,0x10000);//64KB
+  outl(AUDIO_COUNT_ADDR,0);
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = false;
+  cfg->present = true;
+  cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+  outl(AUDIO_FREQ_ADDR, ctrl->freq);
+  outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
+  outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
+  outl(AUDIO_INIT_ADDR,1);
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = 0;
+  stat->count = inl(AUDIO_COUNT_ADDR);
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+  int len = ctl->buf.end - ctl->buf.start;
+  audio_write(ctl->buf.start, len);
 }
--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -1,6 +1,9 @@
+#include "debug.h"
+#include <SDL2/SDL_audio.h>
 #include <common.h>
 #include <device/map.h>
 #include <SDL2/SDL.h>
+#include <stdint.h>
 
 enum {
   reg_freq,
@@ -9,13 +12,59 @@ enum {
   reg_sbuf_size,
   reg_init,
   reg_count,
+  reg_qs,
+  reg_qe,
   nr_reg
 };
 
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
 
+static void audio_play(void *userdata, uint8_t *stream, int len) {
+  int nread = len;
+  if (audio_base[reg_count] < len) nread = audio_base[reg_count];
+
+  int i, start = audio_base[reg_qs];
+  for (i = 0; i < nread; ++i) {
+    stream[i] = sbuf[start];
+    start = (start + 1) % CONFIG_SB_SIZE;
+  }
+  audio_base[reg_qs] = start;
+
+  audio_base[reg_count] -= nread;
+  if (len > nread) {
+    memset(stream + nread, 0, len - nread);
+  }
+}
+
+static void init_AU_SDL() {
+  //init
+  SDL_AudioSpec s = {};
+  s.format = AUDIO_S16SYS;
+  s.userdata = NULL;
+  s.freq = audio_base[reg_freq];
+  s.channels = audio_base[reg_channels];
+  s.samples = audio_base[reg_samples];
+
+  s.callback = audio_play;
+
+  int ret = SDL_InitSubSystem(SDL_INIT_AUDIO);
+  if (ret == 0) {
+    SDL_OpenAudio(&s, NULL);
+    SDL_PauseAudio(0);
+  } else {
+    panic("INIT SDL AUDIO FAIL!");
+  }
+}
+
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
+  switch (offset) {
+    case reg_init << 2 : {
+      return init_AU_SDL();
+    }
+    default:
+      return;
+  }
 }
 
 void init_audio() {
